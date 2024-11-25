b1997cc06e4f18f0138702c73624db3c6b9308ca
AndreaP2A <166503380+AndreaP2A@users.noreply.github.com>
2024-11-25 17:21:10 +0000

    Host component

--- src/components/host/host.jsx
+++ src/components/host/host.jsx
@@ -1,1 +1,14 @@
 import "./_host.scss";
+
+function Host({ host }) {
+  return (
+    <div className="host">
+      <div className="host__name">{host.name}</div>
+      <div className="host__avatar">
+        <img src={host.picture} alt={host.name} />
+      </div>
+    </div>
+  );
+}
+
+export default Host;
--- src/pages/house/house.jsx
+++ src/pages/house/house.jsx
@@ -1,5 +1,6 @@
 import "./_house.scss";
 import Rating from "../../components/rating/rating";
+import Host from "../../components/host/host";
 // import Carousel from "../../components/carousel/carousel";
 import Tag from "../../components/tag/tag";
 // import Collapse from "../../components/collapse/collapse";
@@ -9,6 +10,7 @@ function House({ house }) {
     <div className="house">
       <Rating rating={house.rating} />
       <Tag tags={house.tags} />
+      <Host host={house.host} />
     </div>
   );
 }
